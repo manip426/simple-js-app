f199797300b86744b686b40161693c4752fe0d75
Manpreet Dhaliwal <manip426@gmail.com>
2021-05-18 11:49:08 -0700

    array to hold pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-alert('Hello world');
+//array to hold pokemon list
+let pokemonList = [{name: "Charmander", height: 0.6, types: ["rock", "ground"]},
+                   {name: "Hypno", height: 1.6, type: ["ghost"]},
+                   {name: "Bulbasaur", height: 0.7, type: ["grass"]}];
 
-let favoritefood = 'pizza';
-document.write(favoritefood);
+console.log(pokemonList);
