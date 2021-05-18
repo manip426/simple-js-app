5cd7b87b790b6af6c741efa2b6460edb9c49794e
Manpreet Dhaliwal <manip426@gmail.com>
2021-05-18 11:56:22 -0700

    added variable called pokemon list, added 3 keys to each pokemon object  name, type, height and array to hold list

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 //array to hold pokemon list
 let pokemonList = [{name: "Charmander", height: 0.6, types: ["rock", "ground"]},
-                   {name: "Hypno", height: 1.6, type: ["ghost"]},
+                   {name: "Hypno", height: 1.6, type: ["dragon"]},
                    {name: "Bulbasaur", height: 0.7, type: ["grass"]}];
 
 console.log(pokemonList);
