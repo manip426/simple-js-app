800cbd7462b56df6b481c844b517aece4b05a574
Manpreet Dhaliwal <manip426@gmail.com>
2021-05-21 15:51:05 -0700

    added link for logo for pokemon page, added link and image for fav icon,  changed title to pokedox, added  search bar and button,  also created for loop and document.write included so the list of pokemon could apear on page

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,14 @@
-//array to hold pokemon list
-let pokemonList = [{name: "Charmander", height: 0.6, types: ["rock", "ground"]},
-                   {name: "Hypno", height: 1.6, type: ["dragon"]},
-                   {name: "Bulbasaur", height: 0.7, type: ["grass"]}];
 
-console.log(pokemonList);
+let pokemon = [
+  {name: "Charmander", height: 10 },
+  {name: "Hypno", height: 16},
+  {name: "Bulbasaur", height: 7}
+];
+
+for (let i=0; i< pokemon.length +1; i++) {
+ if (pokemon[i].height > 10) {
+    document.write(pokemon[i].name + "  height:" + pokemon[i].height + " Wow thats big!" + "<br>")
+ } else {
+    document.write(pokemon[i].name + "  height:" + pokemon[i].height + "<br>")
+ }
+}
