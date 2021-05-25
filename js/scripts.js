3caf1c7f7502307bc71c19992d81c78e4d8dc0a7
Manpreet Dhaliwal <manip426@gmail.com>
2021-05-24 17:31:46 -0700

    added forEach loop instead of for loop to pokemon list and height

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,17 @@
-
 let pokemon = [
-  {name: "Charmander", height: 10 },
-  {name: "Hypno", height: 16},
-  {name: "Bulbasaur", height: 7}
+  {
+    name: "Charmander",
+    height: 10
+  },
+  {
+    name: "Hypno",
+    height: 16
+  },
+  {
+    name: "Bulbasaur",
+    height: 7
+  }
 ];
-
-for (let i=0; i< pokemon.length +1; i++) {
- if (pokemon[i].height > 10) {
-    document.write(pokemon[i].name + "  height:" + pokemon[i].height + " Wow thats big!" + "<br>")
- } else {
-    document.write(pokemon[i].name + "  height:" + pokemon[i].height + "<br>")
- }
-}
+pokemon.forEach(function(user) {
+console.log(user.name + " height " + " wow thats big! ");
+});
